eedb3f68d4ef3742e5a1a94527841a844c784448
Matias Crespillo <matsebc@users.noreply.github.com>
2022-09-09 18:52:06 -0300

    Added very basic router

---
+++ UpstraUIKit/UpstraUIKit/AmityRoute.swift
@@ -0,0 +1,57 @@
+//
+//  AmityRoute.swift
+//  
+//
+//  Created by Matias Crespillo on 9/7/22.
+//
+
+import Foundation
+import UIKit
+
+public enum AmityRoute: Equatable {
+    case explore
+    case post(id: String)
+    case none
+}
+
+class AmityRouter {
+    var rootController: AmityRootViewController?
+    var pendingRoute: AmityRoute {
+        didSet {
+            if canRoute {
+                maybeRoute()
+            }
+        }
+    }
+
+    var canRoute: Bool {
+        didSet {
+            if canRoute {
+                maybeRoute()
+            }
+        }
+    }
+
+    init() {
+        rootController = nil
+        pendingRoute = .none
+        canRoute = false
+    }
+
+    func maybeRoute() {
+        guard let rootController = rootController else {
+            return
+        }
+        switch pendingRoute {
+        case .explore:
+            if rootController.canShowExplore() {
+                rootController.showExplore()
+            }
+        case .post(let id):
+            AmityEventHandler.shared.postDidtap(from: rootController, postId: id)
+        case .none:
+            return
+        }
+        self.pendingRoute = .none
+    }
+}
