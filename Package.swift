00f4520ef431ece9f595fdf05750748e895f2e85
Ilya Panchenko <ilyap@noom.com>
2023-04-20 14:47:19 -0700

    Update `AmitySDK` to latest version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["AmityUIKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AmityCo/Amity-Social-Cloud-SDK-iOS-SwiftPM.git", exact: "5.33.3"),
+        .package(url: "https://github.com/AmityCo/Amity-Social-Cloud-SDK-iOS-SwiftPM.git", exact: "6.6.0"),
         .package(url: "https://github.com/SnapKit/SnapKit.git", exact: "5.0.1"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "0.52.0"),
     ],
